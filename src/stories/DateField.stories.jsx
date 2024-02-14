f9fa03c1bef94bd569d684270d966e1181c986a3
Kai <kmichaelmikhail@gmail.com>
2024-02-14 10:47:25 +0100

    Refactor DateField.stories.jsx to use useState hook for managing multiple dates

--- src/stories/DateField.stories.jsx
+++ src/stories/DateField.stories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { action } from "@storybook/addon-actions";
 import DateField from "../components/Field/DateField";
 
@@ -11,6 +11,22 @@ export default {
   },
 };
 
-export const Date = () => (
-  <DateField label="Date" value="" onChange={action("changed")} />
-);
+export const Date = () => {
+  const [dates, setDates] = useState(["", ""]); // Initialize state with two empty dates
+
+  const handleDateChange = (index) => (event) => {
+    const newDates = [...dates];
+    newDates[index] = event.target.value;
+    setDates(newDates);
+    action("changed")(event);
+  };
+
+  return dates.map((date, index) => (
+    <DateField
+      key={index}
+      label={`Date ${index + 1}`}
+      value={date}
+      onChange={handleDateChange(index)}
+    />
+  ));
+};
